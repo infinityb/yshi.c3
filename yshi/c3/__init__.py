8c63d1cf84d6658ed627ae1d2daa1b9e6fc3c32c
Stacey Ell <stacey.ell@gmail.com>
2013-12-29 14:04:12 -0600

    do exception too, I guess

--- yshi/c3/__init__.py
+++ yshi/c3/__init__.py
@@ -19,13 +19,14 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+例外 = Exception
 布爾  = bool
 真 = True
 假 = False
 表 = list
 
 
-class 不一致的層次結構(Exception):
+class 不一致的層次結構(例外):
     pass
 
 
